bb105dd4fa7a8291e5f0ff724f287b6a9e9490a4
lephuquan <lephuquan2k1@.com>
2023-05-07 20:11:56 +0700

    first commit

---
+++ src/main/java/dtu/capstone_2/backend/repository/RealEstateRepository.java
@@ -0,0 +1,80 @@
+package dtu.capstone_2.backend.repository;
+
+import dtu.capstone_2.backend.entity.RealEstate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+public interface RealEstateRepository extends JpaRepository<RealEstate, Long> {
+
+    @Query("select r from RealEstate r where r.id = :id")
+    public RealEstate findRealEstateById(@Param("id") Long id);
+
+    @Modifying // allow delete
+    @Transactional // allow delete
+    @Query(value = "delete from RealEstate where id = :id")
+//    @Query(value = "delete from RealEstate r where r.businessType.id = 1")
+    public void deleteRealEstate(@Param("id") long id);
+    @Query(value = "SELECT * FROM find_home.real_estate\n" +
+            "where (name_estate like %:keySearch% or address like %:keySearch%) \n" +
+            "and (IF(:district='', 1=1, address like %:district%))\n" +
+            "and (IF(:maxPrice=-1, 1=1, price <= :maxPrice and price >= :minPrice))\n" +
+            "and (IF(:maxSquare=-1, 1=1, area <= :maxSquare and area >= :minSquare))\n" +
+            "and (IF(:businessTypeId=-1, 1=1, business_type_id = :businessTypeId))\n" +
+            "and (IF(:typeDetailId=-1, 1=1, type_detail_id = :typeDetailId))\n", nativeQuery = true)
+    public Page<RealEstate> searchDataPurchase(@Param("keySearch") String keySearch,
+                                       @Param("district") String district,
+                                       @Param("minPrice") double minPrice,
+                                       @Param("maxPrice") double maxPrice,
+                                       @Param("minSquare") double minSquare,
+                                       @Param("maxSquare") double maxSquare,
+                                       @Param("businessTypeId") int businessTypeId,
+                                       @Param("typeDetailId") int typeDetailId,
+                                       Pageable pageable);
+
+    @Query(value = "SELECT * FROM find_home.real_estate\n" +
+            "where (name_estate like %:keySearch% or address like %:keySearch%) \n" +
+            "and (IF(:district='', 1=1, address like %:district%))\n" +
+            "and (IF(:maxPrice=-1, 1=1, rent_cost <= :maxPrice and rent_cost >= :minPrice))\n" +
+            "and (IF(:maxSquare=-1, 1=1, area <= :maxSquare and area >= :minSquare))\n" +
+            "and (IF(:businessTypeId=-1, 1=1, business_type_id = :businessTypeId))\n" +
+            "and (IF(:typeDetailId=-1, 1=1, type_detail_id = :typeDetailId))\n", nativeQuery = true)
+    public Page<RealEstate> searchDataLease(@Param("keySearch") String keySearch,
+                                               @Param("district") String district,
+                                               @Param("minPrice") double minPrice,
+                                               @Param("maxPrice") double maxPrice,
+                                               @Param("minSquare") double minSquare,
+                                               @Param("maxSquare") double maxSquare,
+                                               @Param("businessTypeId") int businessTypeId,
+                                               @Param("typeDetailId") int typeDetailId,
+                                               Pageable pageable);
+
+
+
+
+
+    @Query(value = "SELECT * FROM find_home.real_estate\n" +
+            "inner join find_home.type_detail on real_estate.type_detail_id = type_detail.id\n" +
+            "where \n" +
+            "\ttype_detail.type_detail_name like :typeDetailName \n" +
+            "    and real_estate.price >= :priceOrRentCostStart and real_estate.price <= :priceOrRentCostEnd \n" +
+            "    and real_estate.rent_cost >= :priceOrRentCostStart and real_estate.rent_cost <= :priceOrRentCostEnd  \n" +
+            "    and real_estate.area >= :areaStart and real_estate.area <= :areaEnd \n" +
+            "    and real_estate.address like %:address%;", nativeQuery = true)
+    public List<RealEstate> filterSearchPath(@Param("typeDetailName") String typeDetailName,
+                                             @Param("priceOrRentCostStart") Double priceOrRentCostStart,
+                                             @Param("priceOrRentCostEnd") Double priceOrRentCostEnd,
+                                             @Param("areaStart") float areaStart,
+                                             @Param("areaEnd") float areaEnd,
+                                             @Param("address") String address);
+
+
+
+}
---
+++ src/main/java/dtu/capstone_2/backend/service/RealEstateService.java
@@ -0,0 +1,345 @@
+package dtu.capstone_2.backend.service;
+
+import dtu.capstone_2.backend.entity.*;
+import dtu.capstone_2.backend.exception.NullObjectExeption;
+import dtu.capstone_2.backend.model.*;
+import dtu.capstone_2.backend.repository.ImageRepository;
+import dtu.capstone_2.backend.repository.RealEstateRepository;
+import dtu.capstone_2.backend.repository.TypeDetailRepository;
+import dtu.capstone_2.backend.repository.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.text.Normalizer;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RealEstateService {
+
+    @Autowired
+    private RealEstateRepository realEstateRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ImageRepository imageRepository;
+
+    @Autowired
+    private TypeDetailRepository typeDetailRepository;
+
+    public List<RealEstateModel> getAllRealEstate() throws NullObjectExeption {
+        ModelMapper modelMapper = new ModelMapper();
+        List<RealEstate> realEstateList =  realEstateRepository.findAll();
+        if(realEstateList == null){
+            throw new NullObjectExeption("realEstateList is null from getAllRealEstate()");
+        }
+
+        List<RealEstateModel> realEstateModelList = new ArrayList<>();
+
+        for(RealEstate item : realEstateList){
+
+            User user = realEstateRepository.getReferenceById(item.getId()).getUser();
+            UserModel userModel = modelMapper.map(user, UserModel.class);
+
+            BusinessType businessType = realEstateRepository.getReferenceById(item.getId()).getBusinessType();
+            BusinessTypeModel businessTypeModel = modelMapper.map(businessType, BusinessTypeModel.class);
+
+            TypeDetail typeDetail = realEstateRepository.getReferenceById(item.getId()).getTypeDetail();
+            TypeDetailModel typeDetailModel = modelMapper.map(typeDetail, TypeDetailModel.class);
+
+            List<ImageModel> imageModelList = new ArrayList<>();
+            List<Image> imageList = realEstateRepository.findRealEstateById(item.getId()).getImageList();
+            for(Image imageItem: imageList){
+                ImageModel imageModelItem =modelMapper.map(imageItem, ImageModel.class);
+                imageModelList.add(imageModelItem);
+            }
+
+            RealEstateModel realEstateModel = modelMapper.map(item, RealEstateModel.class);
+
+            realEstateModel.setUserModel(userModel);
+            realEstateModel.setBusinessTypeModel(businessTypeModel);
+            realEstateModel.setTypeDetailModel(typeDetailModel);
+            realEstateModel.setImageModelList(imageModelList);
+
+            realEstateModelList.add(realEstateModel);
+        }
+        return realEstateModelList;
+    }
+
+    public RealEstateModel getRealEstateById(Long id) throws NullObjectExeption {
+        ModelMapper modelMapper = new ModelMapper();
+        RealEstate realEstate =  realEstateRepository.findRealEstateById(id);
+        RealEstateModel realEstateModel = modelMapper.map(realEstate, RealEstateModel.class);
+
+
+        User user = realEstateRepository.getReferenceById(realEstate.getId()).getUser();
+        if(user == null){
+            throw new NullObjectExeption("User is null from getRealEstateById() -> realEstateRepository.getReferenceById(realEstate.getId()).getUser();");
+        }else{
+            UserModel userModel = modelMapper.map(user, UserModel.class);
+            realEstateModel.setUserModel(userModel);
+        }
+
+        BusinessType businessType = realEstateRepository.getReferenceById(realEstate.getId()).getBusinessType();
+        if(businessType == null){
+            throw new NullObjectExeption("businessType is null from getRealEstateById() -> realEstateRepository.getReferenceById(realEstate.getId()).getBusinessType()");
+        }else{
+            BusinessTypeModel businessTypeModel = modelMapper.map(businessType, BusinessTypeModel.class);
+            realEstateModel.setBusinessTypeModel(businessTypeModel);
+        }
+
+
+        TypeDetail typeDetail = realEstateRepository.getReferenceById(realEstate.getId()).getTypeDetail();
+        if(typeDetail == null){
+            throw new NullObjectExeption("typeDetail is null from getRealEstateById() -> realEstateRepository.getReferenceById(realEstate.getId()).getTypeDetail()");
+        }else{
+            TypeDetailModel typeDetailModel = modelMapper.map(typeDetail, TypeDetailModel.class);
+            realEstateModel.setTypeDetailModel(typeDetailModel);
+        }
+
+        List<ImageModel> imageModelList = new ArrayList<>();
+        List<Image> imageList = realEstateRepository.findRealEstateById(realEstate.getId()).getImageList();
+        if(imageList == null){
+            throw new NullObjectExeption("imageList is null from getRealEstateById() -> realEstateRepository.findRealEstateById(realEstate.getId()).getImageList()");
+        }else{
+            TypeDetailModel typeDetailModel = modelMapper.map(typeDetail, TypeDetailModel.class);
+            realEstateModel.setTypeDetailModel(typeDetailModel);
+        }
+        for(Image imageItem: imageList){
+            ImageModel imageModelItem =modelMapper.map(imageItem, ImageModel.class);
+            imageModelList.add(imageModelItem);
+        }
+        realEstateModel.setImageModelList(imageModelList);
+
+        return  realEstateModel;
+    }
+    public String addRealEstate(RealEstateModel realEstateModel) throws ParseException {
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDateTime now = LocalDateTime.now();
+
+        String strDate = dtf.format(now);
+
+        ModelMapper modelMapper = new ModelMapper();
+
+        RealEstate realEstate = modelMapper.map(realEstateModel, RealEstate.class);
+        realEstate.setPostDate(new SimpleDateFormat("yyyy-MM-dd").parse(strDate));
+        realEstateRepository.save(realEstate);
+
+        for(ImageModel imageModelItem: realEstateModel.getImageModelList()){
+            imageRepository.insertImage(imageModelItem.getImage(), realEstate.getId() );
+        }
+        return "Add success";
+
+    }
+
+    public String updateRealEstate(RealEstateModel realEstateModel){
+
+        ModelMapper modelMapper = new ModelMapper();
+
+        RealEstate realEstate = realEstateRepository.findRealEstateById(realEstateModel.getId());
+
+        realEstate.setNameEstate(realEstateModel.getNameEstate());
+        realEstate.setAddress(realEstateModel.getAddress());
+        realEstate.setArea(realEstateModel.getArea());
+        realEstate.setPrice(realEstateModel.getPrice());
+        realEstate.setDepositPrice(realEstateModel.getDepositPrice());
+        realEstate.setPayment(realEstateModel.getPayment());
+        realEstate.setMinRentalPeriod(realEstateModel.getMinRentalPeriod());
+        realEstate.setTitle(realEstateModel.getTitle());
+        realEstate.setDescription(realEstateModel.getDescription());
+        realEstate.setVideo(realEstateModel.getVideo());
+        realEstate.setSalientFeatures(realEstateModel.getSalientFeatures());
+        realEstate.setUsableArea(realEstateModel.getUsableArea());
+        realEstate.setStreetHouse(realEstateModel.getStreetHouse());
+        realEstate.setBalconyDirection(realEstateModel.getBalconyDirection());
+        realEstate.setNumberFloors(realEstateModel.getNumberFloors());
+        realEstate.setNumberBedRooms(realEstateModel.getNumberBedRooms());
+        realEstate.setRentalFloorLocation(realEstateModel.getRentalFloorLocation());
+        realEstate.setNumberToilets(realEstateModel.getNumberToilets());
+        realEstate.setInterior(realEstateModel.getInterior());
+
+        realEstate.setBusinessType(modelMapper.map(realEstateModel.getBusinessTypeModel(), BusinessType.class));
+        realEstate.setTypeDetail(modelMapper.map(realEstateModel.getTypeDetailModel(), TypeDetail.class));
+
+        realEstateRepository.save(realEstate);
+
+        for(ImageModel imageModelItem: realEstateModel.getImageModelList()){
+            imageRepository.insertImage(imageModelItem.getImage(), realEstate.getId() );
+        }
+
+        return "update success";
+    }
+
+    public  String deleteRealEstate(long id){
+        imageRepository.deleteImageByRealEstateId(id);
+        realEstateRepository.deleteRealEstate(id);
+        return "delete success";
+    }
+
+    public List<RealEstateModel> searchData(EstateSearchModel model) throws NullPointerException {
+
+        ModelMapper modelMapper = new ModelMapper();
+
+        Pageable pageable;
+
+        switch ( model.getTypeSort()) {
+            case  -1:
+                pageable = PageRequest.of(0, 1000, Sort.by("id").ascending());
+                break;
+            case  1:
+                pageable = PageRequest.of(0, 1000, Sort.by("post_date").descending());
+                break;
+            case  2:
+                pageable = PageRequest.of(0, 1000, Sort.by("price").ascending());
+                break;
+            case  3:
+                pageable = PageRequest.of(0, 1000, Sort.by("price").descending());
+                break;
+            case  4:
+                pageable = PageRequest.of(0, 1000, Sort.by("area").ascending());
+                break;
+            case  5:
+                pageable = PageRequest.of(0, 1000, Sort.by("area").descending());
+                break;
+            default:
+                pageable = PageRequest.of(0, 1000, Sort.by("id").descending());
+        }
+
+        List<RealEstate> realEstateList =  new ArrayList<>();
+
+        if(model.getBusinessTypeId() == 1){
+             realEstateList = realEstateRepository.searchDataPurchase(model.getKeySearch(), model.getDistrict(),
+                    model.getMinPrice(), model.getMaxPrice(), model.getMinSquare(), model.getMaxSquare(),
+                    model.getBusinessTypeId(), model.getTypeDetailId(), pageable).getContent();
+        } else if (model.getBusinessTypeId() == 2) {
+            realEstateList = realEstateRepository.searchDataLease(model.getKeySearch(), model.getDistrict(),
+                    model.getMinPrice(), model.getMaxPrice(), model.getMinSquare(), model.getMaxSquare(),
+                    model.getBusinessTypeId(), model.getTypeDetailId(), pageable).getContent();
+        }
+
+        if(realEstateList == null){
+            throw new NullPointerException("realEstateList is null from searchData()");
+        }
+
+        List<RealEstateModel> realEstateModelList = new ArrayList<>();
+
+        for(RealEstate item : realEstateList){
+
+            User user = realEstateRepository.getReferenceById(item.getId()).getUser();
+            UserModel userModel = modelMapper.map(user, UserModel.class);
+
+            BusinessType businessType = realEstateRepository.getReferenceById(item.getId()).getBusinessType();
+            BusinessTypeModel businessTypeModel = modelMapper.map(businessType, BusinessTypeModel.class);
+
+            TypeDetail typeDetail = realEstateRepository.getReferenceById(item.getId()).getTypeDetail();
+            TypeDetailModel typeDetailModel = modelMapper.map(typeDetail, TypeDetailModel.class);
+
+            List<ImageModel> imageModelList = new ArrayList<>();
+            List<Image> imageList = realEstateRepository.findRealEstateById(item.getId()).getImageList();
+            for(Image imageItem: imageList){
+                ImageModel imageModelItem =modelMapper.map(imageItem, ImageModel.class);
+                imageModelList.add(imageModelItem);
+            }
+
+            RealEstateModel realEstateModel = modelMapper.map(item, RealEstateModel.class);
+
+            realEstateModel.setUserModel(userModel);
+            realEstateModel.setBusinessTypeModel(businessTypeModel);
+            realEstateModel.setTypeDetailModel(typeDetailModel);
+            realEstateModel.setImageModelList(imageModelList);
+
+            realEstateModelList.add(realEstateModel);
+        }
+        return realEstateModelList;
+    }
+
+    public List<RealEstateModel> QuickSearchEstate(QuickSearchEstateModel model) throws NullPointerException {
+
+        ModelMapper modelMapper = new ModelMapper();
+
+        Pageable pageable;
+
+        switch ( model.getTypeSort()) {
+            case  1:
+                pageable = PageRequest.of(0, 1000, Sort.by("post_date").descending());
+                break;
+            case  2:
+                pageable = PageRequest.of(0, 1000, Sort.by("price").ascending());
+                break;
+            case  3:
+                pageable = PageRequest.of(0, 1000, Sort.by("price").descending());
+                break;
+            case  4:
+                pageable = PageRequest.of(0, 1000, Sort.by("area").ascending());
+                break;
+            case  5:
+                pageable = PageRequest.of(0, 1000, Sort.by("area").descending());
+                break;
+            default:
+                pageable = PageRequest.of(0, 1000, Sort.by("id").descending());
+        }
+
+        List<RealEstate> realEstateList =  new ArrayList<>();
+
+        if(model.getBusinessTypeId() == 1){
+            realEstateList = realEstateRepository.searchDataPurchase("", "",
+                    model.getMinPrice(), model.getMaxPrice(), model.getMinSquare(), model.getMaxSquare(),
+                    model.getBusinessTypeId(), model.getTypeDetailId(), pageable).getContent();
+        } else if (model.getBusinessTypeId() == 2) {
+            realEstateList = realEstateRepository.searchDataLease("", "",
+                    model.getMinPrice(), model.getMaxPrice(), model.getMinSquare(), model.getMaxSquare(),
+                    model.getBusinessTypeId(), model.getTypeDetailId(), pageable).getContent();
+        }
+        if(realEstateList == null){
+            throw new NullPointerException("realEstateList is null from searchData()");
+        }
+
+        List<RealEstateModel> realEstateModelList = new ArrayList<>();
+
+        for(RealEstate item : realEstateList){
+
+            User user = realEstateRepository.getReferenceById(item.getId()).getUser();
+            UserModel userModel = modelMapper.map(user, UserModel.class);
+
+            BusinessType businessType = realEstateRepository.getReferenceById(item.getId()).getBusinessType();
+            BusinessTypeModel businessTypeModel = modelMapper.map(businessType, BusinessTypeModel.class);
+
+            TypeDetail typeDetail = realEstateRepository.getReferenceById(item.getId()).getTypeDetail();
+            TypeDetailModel typeDetailModel = modelMapper.map(typeDetail, TypeDetailModel.class);
+
+            List<ImageModel> imageModelList = new ArrayList<>();
+            List<Image> imageList = realEstateRepository.findRealEstateById(item.getId()).getImageList();
+            for(Image imageItem: imageList){
+                ImageModel imageModelItem =modelMapper.map(imageItem, ImageModel.class);
+                imageModelList.add(imageModelItem);
+            }
+
+            RealEstateModel realEstateModel = modelMapper.map(item, RealEstateModel.class);
+
+            realEstateModel.setUserModel(userModel);
+            realEstateModel.setBusinessTypeModel(businessTypeModel);
+            realEstateModel.setTypeDetailModel(typeDetailModel);
+            realEstateModel.setImageModelList(imageModelList);
+
+            realEstateModelList.add(realEstateModel);
+        }
+        return realEstateModelList;
+    }
+
+}
+
+
